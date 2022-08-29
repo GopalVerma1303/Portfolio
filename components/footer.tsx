16dc502ce5891319e9cba1465702569efddfa839
Gopal Verma <67120469+GopalVerma1303@users.noreply.github.com>
2022-08-29 15:55:32 +0530

    Add files via upload

---
+++ components/footer.tsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <footer className="backdrop-blur-sm bg-opacity-30 sm:h-20 py-5 w-full bottom-0 fixed left-0 bottom-0
+    flex justify-center items-center
+    text-white text-2xl">
+      <div className="max-w-5xl mx-auto flex justify-between">
+        <div className="text-gray-500 text-sm ">
+          <a href="https://reactbricks.com" className="text-white hover:text-pink-600">
+            Made with ❤️ by Gopal Verma
+          </a>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+import React from 'react'
+
+export default function Document() {
+    return (
+        <Html className='light'>
+            <Head>
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
+// dark:bg-gray-900
